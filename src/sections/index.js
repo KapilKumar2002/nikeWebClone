6eeae94bb52960d47c433066f283c8b3e5308fb6
KapilKumar2002 <kapil012002@gmail.com>
2023-09-07 00:39:47 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+}
